857a23847842e71ca5a216474ca1af17c3a06c89
ajama <ajama@kth.se>
2023-02-07 12:18:41 +0100

    add gradle proj

---
+++ src/test/java/TestCI.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCI {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void shouldPass() {
+        assertTrue(true);
+    }
+}
